e3d7badac22dfe768dfc8531a5a4bef8fee0d3de
inzamam-credencesoft <106657147+inzamam-credencesoft@users.noreply.github.com>
2022-10-13 11:57:55 +0530

    Made some changes in UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,167 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useEffect, useState } from "react";
+import watermark from 'watermarkjs';
+import { render } from '@testing-library/react';
 
-function App() {
+export default function App() {
+  const imageToWaterMarkRef = useRef();
+  const [imageToWaterMark, setImageToWaterMark] = useState('');
+  const [prevImageToWaterMark, setPrevImageToWaterMark] = useState('');
+  const [watermarkAdded, setWatermarkAdded] = useState(false);
+  const [preview, setPreview] = useState(null);
+
+  const setPreviewImage = (imageToWaterMark,position)=>{
+    setPrevImageToWaterMark(imageToWaterMark);
+      const options = {
+                init(img) {
+                  img.crossOrigin = 'anonymous'
+                }
+        };
+        if (position === "center") {
+          watermark([imageToWaterMark, '/Logo_Bookone.svg'], options)
+          .image(watermark.image.center(0.6))
+          .then(img => {setPreview(img.src)});
+        }
+        else if (position === "lower right"){
+          watermark([imageToWaterMark, '/Logo_Bookone.svg'], options)
+          .image(watermark.image.lowerRight(0.6))
+          .then(img => {setPreview(img.src)});
+        }
+        else if (position === "upper right"){
+          watermark([imageToWaterMark, '/Logo_Bookone.svg'], options)
+          .image(watermark.image.upperRight(0.6))
+          .then(img => {setPreview(img.src)});
+        }
+        else if (position === "lower left"){
+          watermark([imageToWaterMark, '/Logo_Bookone.svg'], options)
+          .image(watermark.image.lowerLeft(0.6))
+          .then(img => {setPreview(img.src)});
+        }
+        else if (position === "upper left"){
+          watermark([imageToWaterMark, '/Logo_Bookone.svg'], options)
+          .image(watermark.image.upperLeft(0.6))
+          .then(img => {setPreview(img.src)});
+        }
+  }
+
+  const addWaterMark = (imageToWaterMark,position)=>{
+    if (imageToWaterMark){
+      const reader = new FileReader();
+      reader.onloadend = ()=>{
+        setWatermarkAdded(true);
+        setPreviewImage(reader.result,position);
+      }
+      reader.readAsDataURL(imageToWaterMark);
+    }
+  }
+
+  const removeWatermark = ()=>{
+    const prevImage = prevImageToWaterMark;
+    setWatermarkAdded(false);
+    setPreview(prevImage);
+  }
+
+  useEffect(() => {
+    if (imageToWaterMark){
+      const reader = new FileReader();
+      reader.onloadend = ()=>{
+        setWatermarkAdded(false);
+        setPreview(reader.result);
+      }
+      reader.readAsDataURL(imageToWaterMark);
+    }
+    else{
+      setPreview(null);
+    }
+
+  }, [imageToWaterMark])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      <div className="contentContainer">
+          <div className="leftSection">
+            <div className="inputSection">
+              <input ref={imageToWaterMarkRef} style={{display: "none"}} type="file" accept="image/*" onChange={(e)=> {
+                const file = e.target.files[0];
+                if (file && file.type.substring(0,5) === "image") {
+                  setImageToWaterMark(file);
+                }
+                else{
+                  setImageToWaterMark(null);
+                }
+              }} />
+              <div className="input">
+              <button onClick={(e)=>{
+                e.preventDefault();
+                imageToWaterMarkRef.current.click();
+              }}>Browse...</button>
+              <input type="text" value={imageToWaterMark !== null ? imageToWaterMark.name : ''} />
+              </div>
+              <span>Select the image to watermark</span>
+            </div>
+            {watermarkAdded ? imageToWaterMark !== "" ? <button className='addRemoveToggleBtn' onClick={()=> removeWatermark(imageToWaterMark)}>Remove watermark</button> : <button disabled className='addRemoveToggleBtn disabledAddToggleBtn'>Remove watermark</button> : imageToWaterMark !== "" ? <button className='addRemoveToggleBtn' onClick={()=> addWaterMark(imageToWaterMark,"center")}>Add watermark</button> : <button className='addRemoveToggleBtn disabledAddToggleBtn'>Add watermark</button>}
+            <div className="positionsSection">
+              {imageToWaterMark !== "" ? <h3>Select position</h3> : <h3>Select image to enable these options</h3>}
+              {imageToWaterMark !== ""
+              ?
+              <div className="positions">
+                <div className="positionInput">
+                  <input onClick={()=> addWaterMark(imageToWaterMark,"lower right")} type="radio" name="position" />
+                  <span>Lower right</span>
+                </div>
+                <div className="positionInput">
+                  <input onClick={()=> addWaterMark(imageToWaterMark,"upper right")} type="radio" name="position" />
+                  <span>Upper right</span>
+                </div>
+                <div className="positionInput">
+                  <input onClick={()=> addWaterMark(imageToWaterMark,"lower left")} type="radio" name="position" />
+                  <span>Lower left</span>
+                </div>
+                <div className="positionInput">
+                  <input onClick={()=> addWaterMark(imageToWaterMark,"upper left")} type="radio" name="position" />
+                  <span>Upper left</span>
+                </div>
+                <div className="positionInput">
+                  <input onClick={()=> addWaterMark(imageToWaterMark,"center")} type="radio" name="position" />
+                  <span>Center</span>
+                </div>
+              </div>
+              :
+              <div className="positions">
+                <div className="positionInput">
+                  <input disabled type="radio" name="position" />
+                  <span>Lower right</span>
+                </div>
+                <div className="positionInput">
+                  <input disabled type="radio" name="position" />
+                  <span>Top right</span>
+                </div>
+                <div className="positionInput">
+                  <input disabled type="radio" name="position" />
+                  <span>Lower left</span>
+                </div>
+                <div className="positionInput">
+                  <input disabled type="radio" name="position" />
+                  <span>Top left</span>
+                </div>
+                <div className="positionInput">
+                  <input disabled type="radio" name="position" />
+                  <span>Center</span>
+                </div>
+              </div>
+              }
+              {imageToWaterMark !== "" ? <a href={preview} download="myfile.jpeg" className='imageDownloadButton'>Download Image</a> : <a className='disabledAddToggleBtn'>Download Image</a>}
+            </div>
+          </div>
+          <div className="rightSection">
+            <h1>Preview</h1>
+            <div className="previewImgContainer" >
+              {preview ? <img src={preview} /> : <img style={{border: "none",marginTop: "100px"}} src="https://media.tenor.com/0Q5kA1OnDwkAAAAi/load-loading.gif" /> }
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
 
-export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,82 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import './App.css';
+import { useRef, useEffect, useState } from "react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+export default function App() {
+  const imgContainerRef = useRef(null);
+  const [watermark, setWatermark] = useState(true);
+  const [watermarked, setWatermarked] = useState("watermarked");
+
+  useEffect(() => {
+    
+    const imgContainerSelector = imgContainerRef.current.querySelectorAll(
+      ".watermarked"
+    );
+
+    console.log("watermark ", watermark);
+    console.log("imgContainerSelector ", imgContainerSelector[0].dataset);
+    if (watermark) {
+      //To reapeat same watermark on multiple images
+      imgContainerSelector.forEach(element => {
+          element.dataset.watermark = (
+            element.dataset.watermark + "   "
+          ).repeat(300);
+      });
+
+      //To repeat watermasrk on single image
+      // imgContainerSelector.dataset.watermark = (
+      //   imgContainerSelector.dataset.watermark + "   "
+      // ).repeat(300);
+    } else {
+      imgContainerSelector.forEach(element => {
+        element.dataset.watermark = "";
+      });
+      // imgContainerSelector.dataset.watermark = "";
+    }
+  }, [watermark]);
+
+  const removeWatermark = () => {
+    setWatermark(false);
+  };
+
+  const addWaterMark = () => {
+    setWatermark(true);
+  };
+
+  return (
+    <div className="App" ref={imgContainerRef}>
+      <h1>Watermark Image in React using CSS</h1>
+      <h2>Toggle watermark to see some magic happen!</h2>
+
+      <button onClick={removeWatermark}>Remove watermark</button>
+      <button onClick={addWaterMark}>Add watermark</button>
+      <br />
+      <br />
+
+      <div className={watermarked} data-watermark="inzamam">
+        <img
+          src="https://source.unsplash.com/Dm-qxdynoEc/800x799"
+          alt="mushroom"
+        />
+      </div>
+      <div className="watermarked" data-watermark="inzamam">
+        <img
+          src="https://source.unsplash.com/Dm-qxdynoEc/800x799"
+          alt="mushroom"
+        />
+      </div>
+      <div className="watermarked" data-watermark="inzamam">
+        <img
+          src="https://source.unsplash.com/Dm-qxdynoEc/800x799"
+          alt="mushroom"
+        />
+      </div>
+      <div className="watermarked" data-watermark="inzamam">
+        <img
+          src="https://source.unsplash.com/Dm-qxdynoEc/800x799"
+          alt="mushroom"
+        />
+      </div>
+    </div>
+  );
+}
+
